69160be280ab7297513eae49dcba0898fae66d75
Sarbian <Sarbian@sarbian.com>
2014-05-18 10:12:01 +0200

    Change the License

--- ModelMultiParticlePersistFX.cs
+++ ModelMultiParticlePersistFX.cs
@@ -1,11 +1,29 @@
 ﻿/*
- * Author: Sébastien GAGGINI AKA Sarbian, France
- * License: Attribution 4.0 International (CC BY 4.0): http://creativecommons.org/licenses/by/4.0/
+ * Copyright (c) 2014, Sébastien GAGGINI AKA Sarbian, France
+ * All rights reserved.
  * 
- * Thanks to Nothke for all the feature ideas, testing and feedback
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * 
  */
-
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -118,8 +136,9 @@ public class ModelMultiParticlePersistFX : EffectBehaviour
 
     private Shader shader;
 
+    private string node_backup = string.Empty;
 
-    public bool activated = true;
+    private bool activated = true;
 
     public bool showUI = false;
 
@@ -726,12 +745,7 @@ public class ModelMultiParticlePersistFX : EffectBehaviour
 
         list.Add(this);
     }
-
-    public string node_backup = string.Empty;
-
-    //private bool node_backupisLoaded = false;
-
-
+    
     public void Backup(ConfigNode node)
     {
         node_backup = SmokeScreenUtil.WriteRootNode(node);
