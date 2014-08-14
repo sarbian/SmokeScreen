92d389e70f3c0d8b46628d54e9be89db16f5242e
Sarbian <Sarbian@sarbian.com>
2014-08-15 00:01:09 +0200

    2.4.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -37,4 +37,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("2.4.2.0")]
+[assembly: AssemblyFileVersion("2.4.3.0")]
